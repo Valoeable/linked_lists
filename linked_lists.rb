661bd9d24bf65bdefeb073e7780e8afa401d2006
Valoeable <samo.valo3@gmail.com>
2023-01-28 14:41:55 +0100

    Added pop, contains(value), find(value) and to_s methods, tweaked error messages and fixed append before prepend error.

--- linked_lists.rb
+++ linked_lists.rb
@@ -21,7 +21,7 @@ class LinkedList
     def append(value)
         new_node = Node.new(value)
         @head = new_node if @head.nil?
-        @tail.next_node = new_node
+        @tail.next_node = new_node if @tail
         @tail = new_node
     end
 
@@ -41,16 +41,8 @@ class LinkedList
         counter
     end
 
-    def head
-        @head
-    end
-
-    def tail
-        @tail
-    end
-
     def at(index)
-        return 'Error, given index is invalid.' if index.negative?
+        return 'Error: given index is invalid.' if index.negative?
 
         targeted_node = @head
         targeted_index = 0
@@ -60,6 +52,65 @@ class LinkedList
         end
         targeted_node
     end
+
+    def pop
+        return 'Error: zero values found.' if self.size.zero?
+
+        if self.size == 1
+            @head = nil
+            return 
+        end
+        current_node = @head
+        targeted_index = 1
+        previous_size = self.size
+        until targeted_index == previous_size
+            if targeted_index == self.size - 1
+                current_node.next_node = nil
+                targeted_index += 1
+                next
+            else
+                targeted_index += 1
+                current_node = current_node.next_node
+            end
+        end
+        @tail = current_node
+    end
+
+    def contains?(value)
+        current_node = @head
+        until current_node.nil?
+            if current_node.value == value
+                return true
+            else
+                current_node = current_node.next_node
+            end
+        end
+        false
+    end
+
+    def find(value)
+        return 'Error: zero values found.' if self.size.zero?
+
+        current_node = @head
+        targeted_index = 0
+        until current_node.nil?
+            return targeted_index if current_node.value == value
+
+            targeted_index += 1
+            targeted_node = targeted_node.next_node
+        end
+        nil
+    end
+
+    def to_s
+        current_node = @head
+        until current_node.nil?
+            print ("(#{current_node.value}) -> ")
+            current_node = current_node.next_node
+        end
+        p current_node
+    end
 end
 
 list = LinkedList.new
+
